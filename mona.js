b9b59afe7b0ee0b4a36d5148330592346dd75cd1
Mona <emonearth915@gmail.com>
2021-04-14 05:06:11 -0400

    JS W3 D2

---
+++ mona.js
@@ -0,0 +1,35 @@
+
+// addition
+    function addTwo(x, y) {
+    answer = x + y;
+    console.log(answer);
+    }
+    addTwo(2, 4);
+    addTwo(1, 9);
+     
+    
+// substraction
+    function subTwo(x, y) {
+    answer = x - y;
+    console.log(answer);
+    }
+    subTwo(7,3);
+    subTwo(4,2);
+
+
+// Multiple
+    function timeTwo(x, y) {
+    answer = x * y;
+    console.log(answer);
+    }
+    timeTwo(8,2);
+    timeTwo(6,2);
+
+
+// divide
+    function divideTwo(x, y) {
+    answer = x / y;
+    console.log(answer);
+    }
+    divideTwo(9,3);
+    divideTwo(16,2);
